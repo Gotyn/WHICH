73bd14c2e242ff3723fbe8801114444940d8c21d
Kameloter <kameloter@hotmail.nl>
2015-09-09 16:04:40 +0200

    Update 1.19
    added item respawn

--- Project 3D/Assets/Scripts/PickableObject.cs
+++ Project 3D/Assets/Scripts/PickableObject.cs
@@ -3,5 +3,27 @@ using System.Collections;
 
 public class PickableObject : MonoBehaviour {
 
+	Vector3 startPos;
 	public bool pickable = false;
+	Camera cam;
+
+	void Start () {
+		startPos = this.transform.position;
+		cam = Camera.main;
+	}
+
+	void Update () {
+		CamControl();
+	}
+	
+	//check if player is outside screen pos
+	void CamControl()
+	{
+		Vector3 screenCoord = cam.WorldToScreenPoint(this.transform.position);
+		
+		if (screenCoord.x < -20 || screenCoord.x > Screen.width + 20 || screenCoord.y < -20 || screenCoord.y > Screen.height + 20)
+		{
+			transform.position = startPos;
+		}
+	}
 }
