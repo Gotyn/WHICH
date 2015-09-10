4431ff53ee788db7b7f4ec6ffac0b1e388980987
VladislavVidenov <vladislavvidenov95@gmail.com>
2015-09-10 15:03:10 +0200

    Update 1.25
    Death particles and death system added. Improved big and small guy
    prefabs.

---
+++ Project 3D/Assets/Scripts/DeathScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathScript : MonoBehaviour {
+
+    Transform mainTranform;
+    CameraControlScript camControl;
+    [SerializeField]
+    GameObject smokePrefab;
+
+	// Use this for initialization
+	void Start () {
+        camControl = GetComponentInParent<CameraControlScript>();
+        mainTranform = transform.parent;
+	}
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if (hit.gameObject.CompareTag("Enemy"))
+        {
+            GameObject go = Instantiate(smokePrefab, transform.position, Quaternion.identity) as GameObject;
+            Destroy(go, 1);
+            camControl.enabled = false;
+            mainTranform.position = new Vector3(100, 0, 100);
+            StartCoroutine(respawn());
+        }
+    }
+
+    IEnumerator respawn()
+    {
+        yield return new WaitForSeconds(1f);
+        camControl.enabled = true;
+    }
+}
