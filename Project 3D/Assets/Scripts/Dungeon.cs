f1d0006ccdfb55f619ac4fa1153012b6ae81bd79
Aaperi <apattman@gmail.com>
2015-09-10 15:07:14 +0200

    1.26
    Added script for entering dark room/dungeon

---
+++ Project 3D/Assets/Scripts/Dungeon.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dungeon : MonoBehaviour {
+
+	bool s = false;
+	bool b = false;
+	
+	GameObject Lamppu;
+	
+	// Use this for initialization
+	void Start () {
+		Lamppu = GameObject.Find ("Directional Light");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (s && b) {
+			RenderSettings.ambientLight = Color.black;
+			Lamppu.SetActive(false);
+		} else {
+			RenderSettings.ambientLight = Color.black;
+			Lamppu.SetActive(true);
+		}
+	}
+	
+	void OnTriggerEnter(Collider hit)
+	{
+		if (hit.transform.CompareTag ("Big"))
+			b = true;
+		
+		if (hit.transform.CompareTag ("Small"))
+			s = true;
+		
+	}
+	
+	void OnTriggerExit(Collider hit)
+	{
+		if (hit.transform.CompareTag ("Big"))
+			b = false;
+		
+		if (hit.transform.CompareTag ("Small"))
+			s = false;
+		
+		
+	}
+}
