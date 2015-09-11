18e74a8e9c97b08ef9634cb7f21e7f1c265cd18a
Kameloter <kameloter@hotmail.nl>
2015-09-11 10:38:50 +0200

    Update 1.31
    added cutscene

---
+++ Project 3D/Assets/Scripts/CameraScripts/CameraSwitch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera cameraMain;
+	public Camera cameraCutScene;
+
+	// Use this for initialization
+	void Start () {
+		GameObject.FindGameObjectWithTag ("Big").GetComponent<PlayerMovement> ().enabled = false;
+		GameObject.FindGameObjectWithTag ("Small").GetComponent<PlayerMovement> ().enabled = false;
+		cameraCutScene.enabled = true;
+		cameraCutScene.GetComponent<Animation> ().Play ();
+		Invoke ("SwitchToMain", 6.1f);
+
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SwitchToMain () {
+		cameraMain.enabled = true;
+		cameraCutScene.enabled = false;
+		GameObject.FindGameObjectWithTag ("Big").GetComponent<PlayerMovement> ().enabled = true;
+		GameObject.FindGameObjectWithTag ("Small").GetComponent<PlayerMovement> ().enabled = true;
+	}
+}
