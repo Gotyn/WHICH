38e476c17c99ba6416d6affd5e82b41147c7d68d
Kameloter <kameloter@hotmail.nl>
2015-09-11 10:53:08 +0200

    Update 1.32.1
    You can skip cutscene now

--- Project 3D/Assets/Scripts/CameraScripts/CameraSwitch.cs
+++ Project 3D/Assets/Scripts/CameraScripts/CameraSwitch.cs
@@ -5,13 +5,16 @@ public class CameraSwitch : MonoBehaviour {
 
 	public Camera cameraMain;
 	public Camera cameraCutScene;
+	bool isPlaying = false;
 
 	// Use this for initialization
 	void Start () {
 		GameObject.FindGameObjectWithTag ("Big").GetComponent<PlayerMovement> ().enabled = false;
 		GameObject.FindGameObjectWithTag ("Small").GetComponent<PlayerMovement> ().enabled = false;
+		GameObject.FindGameObjectWithTag ("Small").GetComponentInChildren<FireAttackScript> ().enabled = false;
 		cameraCutScene.enabled = true;
 		cameraCutScene.GetComponent<Animation> ().Play ();
+		isPlaying = true;
 		Invoke ("SwitchToMain", 6.1f);
 
 	
@@ -21,6 +24,10 @@ public class CameraSwitch : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (isPlaying && (Input.GetButtonDown ("SMALL_INTERACT_1") || Input.GetButtonDown ("BIG_INTERACT_1"))) {
+			cameraCutScene.GetComponent<Animation> ().Stop ();
+			SwitchToMain();
+		}
 	}
 
 	void SwitchToMain () {
@@ -28,5 +35,6 @@ public class CameraSwitch : MonoBehaviour {
 		cameraCutScene.enabled = false;
 		GameObject.FindGameObjectWithTag ("Big").GetComponent<PlayerMovement> ().enabled = true;
 		GameObject.FindGameObjectWithTag ("Small").GetComponent<PlayerMovement> ().enabled = true;
+		GameObject.FindGameObjectWithTag ("Small").GetComponentInChildren<FireAttackScript> ().enabled = true;
 	}
 }
