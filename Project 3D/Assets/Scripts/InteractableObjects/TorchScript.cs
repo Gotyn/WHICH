7fd4fde472370eabe37cf686b1278626e1ea8f13
Martijn Snip <martijnsnip@gmail.com>
2015-09-14 13:41:27 +0200

    Update 1.47 - Sound
    Fade in fade out on campfires.

--- Project 3D/Assets/Scripts/InteractableObjects/TorchScript.cs
+++ Project 3D/Assets/Scripts/InteractableObjects/TorchScript.cs
@@ -8,14 +8,17 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 
 public class TorchScript : MonoBehaviour {
+    private GameManagerScript gameManager; //to check the average position of the players.
 
 	[SerializeField]
 	private GameObject particles;
     private AudioSource audioFire;
 
+    private float distanceToPlayers;
+
     [HideInInspector]
     public bool isLit = false;
-   
+
     // Used when you need to know in what state the torch needs to be in, in order to complete the puzzle !
     public bool needActivated = false;
     [HideInInspector]
@@ -26,6 +29,8 @@ public class TorchScript : MonoBehaviour {
     void Start()
     {
         audioFire = GetComponent<AudioSource>();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManagerScript>();
+
     }
 
     // Update is called once per frame
@@ -40,4 +45,20 @@ public class TorchScript : MonoBehaviour {
 		isLit = false;
         if (audioFire != null)audioFire.Stop();
 	}
+
+    void Update() {
+        distanceToPlayers = Vector3.Distance(transform.position, gameManager.PlayersAveragePosition);
+        Debug.Log("dis2p " + distanceToPlayers + "   " + name);
+        if (distanceToPlayers > 50 && isLit && audioFire.isPlaying) {
+            audioFire.volume -= 0.1f * Time.deltaTime;
+        }
+
+        if (distanceToPlayers < 50 && isLit && audioFire.isPlaying) {
+            if (audioFire.volume >= 0.2f) {
+                audioFire.volume = 0.2f;
+            } else {
+                audioFire.volume += 0.1f * Time.deltaTime;
+            }
+        }
+    }
 }
