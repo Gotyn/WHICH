c829e8b350405f4032d7405f1f9510c5a14c7f41
Martijn Snip <martijnsnip@gmail.com>
2015-09-14 16:32:05 +0200

    Update 1.49.3 - Menu
    More changes in menu. Not finished yet.

--- Project 3D/Assets/Scripts/menuScript.cs
+++ Project 3D/Assets/Scripts/menuScript.cs
@@ -3,23 +3,21 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class menuScript : MonoBehaviour {
-    public Canvas menu;
-    public Canvas exitMenu;
-    public Canvas pauseMenu;
-    public Canvas quitMenu;
+    public Canvas menu, 
+                  exitMenu, 
+                  pauseMenu, 
+                  quitMenu;
+    public Button playButton, 
+                  continueButton, 
+                  exitButton, 
+                  quitButton;
 
-    public Button playButton;
-    public Button continueButton;
-    public Button exitButton;
-    public Button quitButton;
-
-    public bool canvasOn;
-    public bool paused;
+    public bool canvasOn, paused;
 	
 
     CameraSpline camSpline;
-    PlayerMovement sBroMovement;
-    PlayerMovement bBroMovement;
+
+    PlayerMovement sBroMovement, bBroMovement;
 
 	public float volume =1;
 	Slider volumeslider; 
