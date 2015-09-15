5e4c7067c1662425778ba52ccba7a07a842be5f6
Kameloter <kameloter@hotmail.nl>
2015-09-15 10:51:39 +0200

    Update 1.52
    added soccer game in different scene

---
+++ Project 3D/Assets/Scripts/SoccerScripts/SGoalScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SGoalScript : MonoBehaviour {
+
+	public bool bGoal= false;
+
+	GameObject soccerManager;
+		
+	GameObject big;
+	GameObject small;
+
+	void Start () {
+		soccerManager = GameObject.Find("SoccerManager");
+		big = GameObject.FindGameObjectWithTag ("Big");
+		small = GameObject.FindGameObjectWithTag ("Small");
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Ball")) {
+			if (bGoal) {
+				soccerManager.GetComponent<ScoreScript>().bScore++;
+			} else{
+				soccerManager.GetComponent<ScoreScript>().sScore++;
+			}
+			big.GetComponent<CameraControlScript>().Respawn();
+			small.GetComponent<CameraControlScript>().Respawn();
+			soccerManager.GetComponent<ScoreScript>().Respawn();
+
+		}
+
+	}
+}
