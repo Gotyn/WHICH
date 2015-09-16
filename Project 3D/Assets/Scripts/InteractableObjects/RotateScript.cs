ac9e3fc46e86032cfdfef64e3cca7112e2e6c75d
Kameloter <kameloter@hotmail.nl>
2015-09-16 11:54:20 +0200

    Update 2.4
    added scripts for rotating for rotate things

---
+++ Project 3D/Assets/Scripts/InteractableObjects/RotateScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateScript : MonoBehaviour {
+
+	public int rotateSpeed = 1;
+
+	void Update () {
+	
+		this.transform.Rotate (0, rotateSpeed, 0);
+	}
+
+	void OnCollisionEnter (Collision other) {
+		if (other.collider.CompareTag ("Big") || other.collider.CompareTag ("Small")) {
+			other.gameObject.GetComponent<CameraControlScript>().Respawn();
+		}
+	}
+}
