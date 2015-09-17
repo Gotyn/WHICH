891947fc3791cfba11bce26571db7b120ff38e0b
VladislavVidenov <vladislavvidenov95@gmail.com>
2015-09-17 10:44:46 +0200

    Update 3 - bla.

---
+++ Project 3D/Assets/Scripts/Log.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Log : MonoBehaviour {
+
+    [SerializeField]
+    private Transform com;
+
+	void Start()
+    {
+        GetComponentInChildren<Rigidbody>().centerOfMass = com.position;
+        Debug.Log(transform.parent.name);
+    }
+}
---
+++ Project 3D/Assets/Scripts/LogEncounter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogEncounter : MonoBehaviour {
+
+    bool eventStarted = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider hit)
+    {
+        if(hit.CompareTag("Small") || hit.CompareTag("Big"))
+        {
+            if (!eventStarted) StartCoroutine(logEncounter());
+
+        }
+    }
+
+    IEnumerator logEncounter()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+    }
+
+    void spawnLog()
+    {
+
+    }
+}
