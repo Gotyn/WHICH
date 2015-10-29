d2c146b37f613b100e150bc6da5cd4a3810220ec
Kameloter <kameloter@hotmail.nl>
2015-10-30 00:01:22 +0100

    Update beta 1.06.6
    added pp sound

--- Project 3D/Assets/Scripts/Environmental/Objects/Movable/PressurePlateMovement.cs
+++ Project 3D/Assets/Scripts/Environmental/Objects/Movable/PressurePlateMovement.cs
@@ -3,8 +3,32 @@ using System.Collections;
 
 public class PressurePlateMovement : InteractableObjectMovement
 {
-    void Start()
+
+	private AudioSource audioPlate;
+	
+	private int previousState;
+
+	void Start()
     {
+		audioPlate = GetComponent<AudioSource>();
         maxDistance = 0.05f;
     }
+
+	void Update () {
+		ManageAudio ();
+	}
+
+	void PlaySound () {
+		GetComponent<AudioSource> ().Play ();
+	}
+
+	void ManageAudio() {
+		if (audioPlate != null && !audioPlate.isPlaying && previousState != state) {
+			if (state == 2 || state == 1) {
+				audioPlate.Play();
+				previousState = state;
+			}
+			  //set currentstate as previousstate to prevent soundloop.
+		}
+	}
 }
