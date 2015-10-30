4107dfde9853fc76ea7109586d81267882971fe5
Martijn Snip <martijnsnip@gmail.com>
2015-10-30 21:43:55 +0100

    BETA 1.2.41
    Tried something for Hint system.

---
+++ Project 3D/Assets/Scripts/UI/InteractionHintScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InteractionHintScript : MonoBehaviour {
+
+    [SerializeField]
+    Image explanationImage;
+
+    // Use this for initialization
+	void Start () {
+        explanationImage.enabled = false;
+	}
+    
+    void OnTriggerEnter() {
+        explanationImage.enabled = true;
+    }	
+
+    void OnTriggerExit() {
+        explanationImage.enabled = false;
+    }
+}
