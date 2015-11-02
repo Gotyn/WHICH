3e84cebbbee64a89f58d8a3468131ca5a6bc0ca4
VladislavVidenov <vladislavvidenov95@gmail.com>
2015-11-02 12:26:16 +0100

    Beta 1.4

---
+++ Project 3D/Assets/Scripts/Environmental/Objects/QuestionMarkRotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuestionMarkRotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 2.5f, 0);
+	}
+}
