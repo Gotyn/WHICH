4d2268ae65efaaec1c18c74414eaf750bec1941a
Kameloter <kameloter@hotmail.nl>
2015-11-12 02:45:47 +0100

    WHICH 2.3
    scenes upadate

--- Project 3D/Assets/Scripts/UI/DialogueScript.cs
+++ Project 3D/Assets/Scripts/UI/DialogueScript.cs
@@ -269,7 +269,7 @@ public class DialogueScript : MonoBehaviour {
 
     IEnumerator Puzzle_9(float delay) {
         if (!menu.dialoguesEnabled) yield break;
-        yield return new WaitForSeconds(delay);
+        yield return new WaitForSeconds(4);
         chat.enabled = true;
 
         //Start
